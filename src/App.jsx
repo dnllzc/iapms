e947fd75b1f1467f9c3160bc7ef330fdf50e4ff1
dnllzc <89221006@student.upr.si>
2026-05-09 15:13:28 +0200

    Initial project commit
    Initialization of the project with Vite and React

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className="center">
+        <div className="hero">
+          <div className="hero__content">
+            <h1 className="hero__title">IAPMS</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
