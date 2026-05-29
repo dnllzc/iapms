cd941d2876e7d6343012eff82d945030095186b7
Danilo Lazic <89221006@student.upr.si>
2026-05-29 15:01:12 +0200

    Admin: Discount Code functionalities
    Fully implemented creation of new discount codes with proper endpoints in backend for database updates and also deletion of codes fully implemented

--- back-end/routes/discountcodes.js
+++ back-end/routes/discountcodes.js
@@ -13,11 +13,48 @@ const getDiscountCodes = () => {
     });
 }
 
+const createDiscountCode = (code, discount_type, value, expiration_date) => {
+    return new Promise((resolve, reject) => {
+        conn.query('INSERT INTO discount_code (code, discount_type, value, expiration_date) VALUES (?, ?, ?, ?)', [code, discount_type, value, expiration_date], (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            } 
+            resolve(result);
+        });
+    });
+}
+
 const router = Router();
 router.get('/', async (req, res, next) => {
     try {
         const discountCodes = await getDiscountCodes();
         res.json(discountCodes);
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.post('/new', async (req, res, next) => {
+    try {
+        const { code, discount_type, value, expiration_date } = req.body;
+        const result = await createDiscountCode(code, discount_type, value, expiration_date);
+        res.status(201).json(result);
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.post('/delete', async (req, res, next) => {
+    try {
+        const { id } = req.body;
+        conn.query('DELETE FROM discount_code WHERE id = ?', [id], (err, result) => {
+            if (err) {
+                next(err);
+                return;
+            } 
+            res.json(result);
+        });
     } catch (err) {
         next(err);
     }
