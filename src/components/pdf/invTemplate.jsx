22b6ab260aee83acffa1727219bbb500b8399c14
Danilo Lazic <89221006@student.upr.si>
2026-06-03 14:21:30 +0200

    Start of implementing print to pdf
    Currently created stylesheet for invoice template for printing to pdf based on an old way of printing invoices while working in a company. Will implement it using window.print with hidden innerHtml

---
+++ src/components/pdf/invTemplate.jsx
@@ -0,0 +1,11 @@
+import './invTemplate.css';
+
+export default function InvTemplate() {
+  return (
+    <>
+        <section className="inv-template">
+            <h1>Invoice Template</h1>
+        </section>
+    </>
+  );
+}
